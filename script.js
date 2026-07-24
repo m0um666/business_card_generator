38f0128f15cd0267b7da85444b44973303fe99ee
Mouma <negrito13011@protonmail.com>
2026-07-24 14:59:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ document.getElementById("downloadBtn").addEventListener("click", async function(
 
     const element = document.getElementById("cardPreview");
 
-
-    // Clone pour capture
+    // Créer une copie visible pour html2canvas
     const clone = element.cloneNode(true);
 
     clone.style.display = "block";
@@ -13,13 +12,9 @@ document.getElementById("downloadBtn").addEventListener("click", async function(
     clone.style.top = "0";
     clone.style.background = "white";
     clone.style.color = "black";
-    clone.style.width = "350px";
-    clone.style.height = "200px";
-    clone.style.opacity = "1";
-
+    clone.style.width = "280px";
 
     document.body.appendChild(clone);
-
 
 
     await new Promise(resolve => setTimeout(resolve, 300));
@@ -27,58 +22,36 @@ document.getElementById("downloadBtn").addEventListener("click", async function(
 
     try {
 
-
         const canvas = await html2canvas(clone, {
-
             backgroundColor: "#ffffff",
-
-            scale: 3
-
+            scale: 2
         });
-
 
 
         const imgData = canvas.toDataURL("image/png");
 
 
-
         const { jsPDF } = window.jspdf;
 
 
-
-        // Vraie taille carte de visite
         const pdf = new jsPDF({
-
-            orientation: "landscape",
-
-            unit: "mm",
-
-            format: [85, 55]
-
+            orientation: "portrait",
+            unit: "px",
+            format: [canvas.width, canvas.height]
         });
 
 
-
         pdf.addImage(
-
             imgData,
-
             "PNG",
-
             0,
-
             0,
-
-            85,
-
-            55
-
+            canvas.width,
+            canvas.height
         );
 
 
-
         pdf.save("carte_de_visite.pdf");
-
 
 
     } catch(error) {
@@ -88,9 +61,8 @@ document.getElementById("downloadBtn").addEventListener("click", async function(
     }
 
 
-
+    // Supprimer la copie
     clone.remove();
-
 
 });
 
